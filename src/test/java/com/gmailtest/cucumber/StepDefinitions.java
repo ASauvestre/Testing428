a84413fb3b2d87e6a7d641f518793686232c82cb
Dibbo <dibbor@users.noreply.github.com>
2019-03-09 19:11:51 -0500

    normal flow

--- src/test/java/com/gmailtest/cucumber/StepDefinitions.java
+++ src/test/java/com/gmailtest/cucumber/StepDefinitions.java
@@ -5,14 +5,15 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.*;
 
 public class StepDefinitions {
     private WebDriver driver;
@@ -27,10 +28,14 @@ public class StepDefinitions {
     private final String USER_MAIL = "practicetesting4@gmail.com";
     private final String USER_PASSWORD = "ecse428-";
     private final String COMPOSE_URL= "https://mail.google.com/mail/u/0/#inbox?compose=new";
+    private final String TO_XPATH= "//*[@aria-label=\"To\"]";
     private final String COMPOSE_XPATH="//*[@aria-label=\"Navigate to\"]/../../../div[1]/div/div/div/div/div/div";
     private final String SUBJECT_XPATH="//*[@aria-label=\"Subject\"]";
     private final String ATTACH_XPATH="//*[@aria-label=\"Attach files\"]/div/div/div";
-    private final String SEND_KEY="//*[contains(@aria-label,'Send')]";
+    private final String SEND_KEY="//*[contains(@aria-label,'Send ')]";
+    private final String IMAGE_NAME= "GGMU.JPEG";
+    private final String SENT_XPATH="//*[@aria-label=\"Sent\"]/../../..";
+    public String subject_text;
 
     @Given("I am logged in")
     public void iAmLoggedIn() {
@@ -66,38 +71,81 @@ public class StepDefinitions {
     //doesn't work
     @And("I compose an email to {string}")
     public void iComposeAnEmailTo(String arg0) {
+        generateString();
         setupSeleniumWebDrivers();
+        WebElement to = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(TO_XPATH)));
         System.out.println("before subject");
-        WebElement random = (new WebDriverWait(driver, 20)).until(ExpectedConditions.elementToBeClickable(By.xpath(SUBJECT_XPATH)));
-        random.sendKeys(arg0);
-       // System.out.println("before to");
-
-
-
-
-
-
+        to.sendKeys(arg0);
+        WebElement subject = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(SUBJECT_XPATH)));
+        subject.sendKeys(subject_text);
+        System.out.println("before to");
 
     }
 
     @And("I attach a picture")
-    public void iAttachAPicture() {
+    public void iAttachAPicture() throws InterruptedException, AWTException {
         driver.findElement(By.name("Filedata")).sendKeys("C:\\Users\\dibbo\\OneDrive\\Desktop\\GGMU.JPEG");
+        System.out.println("before sleep");
+        Robot robot= new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+
 
 
     }
 
     //trying to make it work
     @Then("the email should be sent")
-    public void theEmailShouldBeSent() throws InterruptedException {
-        System.out.println("before sleep");
-        TimeUnit.SECONDS.sleep(20);
-        System.out.println("after sleep");
-        WebElement send = (new WebDriverWait(driver, 20)).until(ExpectedConditions.elementToBeClickable(By.xpath(SEND_KEY)));
-        send.click();
+    public void theEmailShouldBeSent() throws InterruptedException, AWTException {
+        WebElement sent_button = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath(SENT_XPATH)));
+        sent_button.click();
+
+
+        if ( driver.findElement(By.xpath("//div[text()='GGMU.JPEG']")) != null) {
+            if( driver.findElement(By.xpath("//div[text()='" + subject_text + "']")) != null){
+                System.out.println("Email was sent successfully!");
+                WebElement mail = null;
+                while(mail == null) {
+                    WebDriverWait wait = new WebDriverWait(driver, 10);
+                    mail = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@email=\"adrien.sauvestre@mail.mcgill.ca\"]/../..")));
+                }
+                mail.click();
+            }
+
+        }
+        else {
+            System.out.println("Email failed to send!");
+        }
+
+        WebElement inbox = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@aria-label,'Inbox')]")));
+        inbox.click();
+        WebElement SignOut = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@aria-label,'Google Account: Garbage Practice')]")));
+        SignOut.click();
+        WebElement SignOut_button = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.id("gb_71")));
+        SignOut_button.click();
+
+        driver.switchTo().alert().accept();
+
+
     }
 
+
     // Helper functions
+
+    public void generateString() {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 12) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        subject_text = salt.toString();
+
+    }
     private void setupSeleniumWebDrivers() {
         if (driver == null) {
             System.out.println("Setting up ChromeDriver... ");
--- src/test/resources/gmail.feature
+++ src/test/resources/gmail.feature
@@ -4,7 +4,9 @@ Feature: Gmail
     Given I am logged in
     And I am on the Gmail main page
     When I press "Compose"
-    And I compose an email to "dibbo.ritwik@mail.mcgill.ca"
+    And I compose an email to "adrien.sauvestre@mail.mcgill.ca"
     And I attach a picture
     And I press "Send"
     Then the email should be sent
+
+
